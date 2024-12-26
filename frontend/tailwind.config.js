dda485a0f5611bc9281b529adb453a9a84e49d66
Shrutika Jadhav <shrutikajad432@gmail.com>
2024-12-26 23:47:02 +0530

    project completed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    "themes":["dark", "light", "cupcake", "bumblebee", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "wireframe", "black", "luxury", "dracula", "synthwave", "hacker", "gothic", "neon", "corporate", "gruvbox", "red", "vaporwave", "island", "papercss", "tailwind", "bootstrap", "material", "tailwindcss", "bulma", "semantic", "bootstrap", "cyberpunk", "dark", "darker", "palenight", "dracula", "tomorrow", "up", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "wireframe", "black", "luxury", "dracula", "synthwave", "hacker", "gothic", "neon", "corporate", "gruvbox", "red", "vaporwave", "island", "papercss", "tailwind", "bootstrap", "material", "tailwindcss", "bulma", "semantic", "bootstrap", "cyberpunk", "dark", "darker", "palenight", "dracula", "tomorrow", "up", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "wireframe", "black", "luxury", "dracula", "synthwave", "hacker", "gothic", "neon", "corporate", "gruvbox", "red", "vaporwave", "island", "papercss", "tailwind", "bootstrap", "material", "tailwindcss", "bulma", "semantic", "bootstrap", "cyberpunk", "dark", "darker", "palenight", "dracula", "tomorrow", "up", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "wireframe", "black", "luxury", "dracula", "synthwave", "hacker", "gothic", "neon", "corporate", "gruvbox", "red", "vaporwave", "island", "papercss", "tailwind",],
+  }
+}
